efdacd9aad8cc59e74bb3b8d6560346dd543d423
nbHelga <160697257+nbHelga@users.noreply.github.com>
2024-03-10 22:46:51 +0700

    Add files via upload

---
+++ Pertemuan 3/Tugas Tambahan/Exception/Buku.java
@@ -0,0 +1,25 @@
+/* 
+ * Penulis : Helga Nurul Bhaiti / 24060122130062
+ */
+
+public class Buku {
+    private String judul;
+    private boolean tersedia;
+    
+    public Buku(String judul) {
+        this.judul = judul;
+        tersedia = true;
+    }
+    public String getJudul() {
+        return judul;
+    }
+    public boolean isTersedia() {
+        return tersedia;
+    }
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+    public void setTersedia(boolean tersedia) {
+        this.tersedia = tersedia;
+    }
+}
